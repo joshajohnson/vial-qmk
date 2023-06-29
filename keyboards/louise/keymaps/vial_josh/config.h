bfc4cd32870e9bc31b41e75bd86b46b0109d4f32
Josh Johnson <josh@joshajohnson.com>
2023-06-29 19:17:38 +1000

    add unlock

--- keyboards/louise/keymaps/vial_josh/config.h
+++ keyboards/louise/keymaps/vial_josh/config.h
@@ -1,4 +1,8 @@
 #define VIAL_KEYBOARD_UID {0xFD, 0x1A, 0x24, 0x0D, 0x43, 0x50, 0x19, 0xBC}
 
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 19 }
+
 // Time out one shot layers after 3 seconds
 #define ONESHOT_TIMEOUT 3000
+
