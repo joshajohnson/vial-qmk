68208f6bf242f07a0c28d15c89da0fb86d7bdc0d
Josh Johnson <josh@joshajohnson.com>
2025-09-29 23:08:48 +1000

    fun fact: building against latest works!

---
+++ keyboards/split60/config.h
@@ -0,0 +1,6 @@
+// Copyright 2025 Josh Johnson
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define SPLIT_HAND_PIN_LOW_IS_LEFT
---
+++ keyboards/split60/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+// Copyright 2025 Josh Johnson
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x49, 0x09, 0xEC, 0x00, 0xDB, 0x77, 0xCD, 0x0F}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 7 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 7 }
